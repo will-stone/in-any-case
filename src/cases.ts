ac977448298f970383feeda4744002b04a8e6101
Will Stone <654103+will-stone@users.noreply.github.com>
2020-05-18 20:35:44 +0100

    Fix tests

--- src/cases.ts
+++ src/cases.ts
@@ -20,7 +20,7 @@ const CASES: Case[] = [
     changer: (string: string): string => camelCase(string),
     commandId: 'camelCase',
     testInput: 'lorem ipsum Dolor sit_Amet',
-    testOutput: 'loremIpsumDolorSitmet',
+    testOutput: 'loremIpsumDolorSitAmet',
   },
   {
     changer: (string: string): string => capitalize(string),
